0f00509ce8c29c6855502835ff19fa407dcf7629
anoop <anoop.emsl@gmail.com>
2020-08-27 13:29:48 +0530

    Revert "after stash and conflict erase"
    This reverts commit 9c3a0bd8c9cb328e5d66698e76bac47e15a31dc5.

--- firstclass.java
+++ firstclass.java
@@ -1,8 +1,4 @@
-Class test
 
-//hey repo
-//
-//this is the commit i have done to show workin of stash
-//
-//this is the modification i want to have'
 
+
+this is the commit i have done to show workin of stash
